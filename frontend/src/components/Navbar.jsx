c1e09054e7c3bfff36d1d4f751a0ea640f9aa254
Manalbariz <manal.bariz@epitech.eu>
2023-10-12 15:28:25 +0200

    home page

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,12 +1,12 @@
 import { Link } from 'react-router-dom';
-
+import LogoML from '../assets/HomeImgs/logo_ml.png';
 
 const Navbar = () => {
     return (
         <header>
             <div className="container">
                 <Link to="/" className="logo">
-                    <h1>M&L Jobs</h1>
+                    <img src={LogoML} alt="logo" className='logoMl' />  
                 </Link>
                 <button className="contactezNousBtt">Contacter-nous</button>
             </div>
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,7 +3,7 @@ import BgImg from '../assets/HomeImgs/BgImg.jpeg'
 const Home = () => {
     return (
    
-            <div className='home' style={{ backgroundImage: `url(${BgImg})`, backgroundRepeat: 'no-repeat', backgroundSize: 'cover', height: '85vh' }}>
+            <div className='BgHome' style={{ backgroundImage: `url(${BgImg})`, backgroundRepeat: 'no-repeat', backgroundSize: 'cover', opacity:1}}>
                 <h1 className="bigtitle">Trouvez exactement ce que vous cherchez</h1>
                 <p className="intro">Nous sommes là pour vous fournir la plus haute qualité de service</p>
                 <button className="commencerBtt">Commencer</button>
